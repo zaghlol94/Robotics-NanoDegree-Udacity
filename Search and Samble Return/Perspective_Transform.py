32bb4939d5e6480c457b26dc89899a197ae589e7
zaghlol94 <zaghlollight@gmail.com>
2018-09-03 13:04:21 +0200

    research and sample

---
+++ Search and Samble Return/Perspective_Transform.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 31 13:02:10 2018
+
+@author: zaghlollight
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+
+image = mpimg.imread('1.jpg')
+
+# Rover yaw values will come as floats from 0 to 360
+# Generate a random value in this range
+# Note: you need to convert this to radians
+    # before adding to pixel_angles
+rover_yaw = np.random.random(1)*360
+
+# Generate a random rover position in world coords
+# Position values will range from 20 to 180 to 
+# avoid the edges in a 200 x 200 pixel world
+rover_xpos = np.random.random(1)*160 + 20
+rover_ypos = np.random.random(1)*160 + 20
+
+
+def perspect_transform(img, src, dst):
+
+    # Get transform matrix using cv2.getPerspectivTransform()
+    M = cv2.getPerspectiveTransform(src, dst)
+    # Warp image using cv2.warpPerspective()
+    # keep same size as input image
+    warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))
+    # Return the result
+    return warped
+
+
+def color_thresh(img, rgb_thresh=(0, 0, 0)):
+    ###### TODO:
+    
+    # Create an empty array the same size in x and y as the image 
+    # but just a single channel
+    color_select = np.zeros_like(img[:,:,0])
+    # Apply the thresholds for RGB and assign 1's
+    above_thresh = (img[:,:,0] > rgb_thresh[0]) \
+                & (img[:,:,1] > rgb_thresh[1]) \
+                & (img[:,:,2] > rgb_thresh[2])    
+    color_select[above_thresh]=1
+    # where threshold was exceeded
+    # Return the single-channel binary image
+    return color_select
+
+# Define a function to convert from image coords to rover coords
+def rover_coords(binary_img):
+    # Identify nonzero pixels
+    ypos, xpos = binary_img.nonzero()
+    # Calculate pixel positions with reference to the rover position being at the 
+    # center bottom of the image.  
+    x_pixel = -(ypos - binary_img.shape[0]).astype(np.float)
+    y_pixel = -(xpos - binary_img.shape[1]/2 ).astype(np.float)
+    return x_pixel, y_pixel
+
+def rotate_pix(xpix, ypix, yaw):
+    # Convert yaw to radians
+    yaw_rad = yaw * np.pi / 180
+    xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))
+                            
+    ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
+    # Return the result  
+    return xpix_rotated, ypix_rotated
+
+# Define a function to perform a translation
+def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
+    # TODO:
+    # Apply a scaling and a translation
+    xpix_translated = (xpix_rot / scale) + xpos
+    ypix_translated = (ypix_rot / scale) + ypos
+    # Return the result  
+    return xpix_translated, ypix_translated
+
+# Define a function to apply rotation and translation (and clipping)
+# Once you define the two functions above this function should work
+def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
+    # Apply rotation
+    xpix_rot, ypix_rot = rotate_pix(xpix, ypix, yaw)
+    # Apply translation
+    xpix_tran, ypix_tran = translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale)
+    # Clip to world_size
+    x_pix_world = np.clip(np.int_(xpix_tran), 0, world_size - 1)
+    y_pix_world = np.clip(np.int_(ypix_tran), 0, world_size - 1)
+    # Return the result
+    return x_pix_world, y_pix_world
+# TODO:
+# Define a box in source (original) and 
+# destination (desired) coordinates
+# Right now source and destination are just 
+# set to equal the four corners
+# of the image so no transform is taking place
+# Try experimenting with different values!
+dst_size = 5 
+bottom_offset = 6
+source = np.float32([[14, 140] ,[301 ,140],[200, 96], [118, 96]]) 
+destination = np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_offset], 
+                 [image.shape[1]/2 + dst_size, image.shape[0] - bottom_offset], 
+                 [image.shape[1]/2 + dst_size, image.shape[0] - 2*dst_size - bottom_offset], 
+                 [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset]])
+
+warped = perspect_transform(image, source, destination)
+
+# Draw Source and destination points on images (in blue) before plotting
+cv2.polylines(image, np.int32([source]), True, (0, 0, 255), 3)
+cv2.polylines(warped, np.int32([destination]), True, (0, 0, 255), 3)
+# Display the original image and binary               
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6), sharey=True)
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=40)
+
+ax2.imshow(warped, cmap='gray')
+ax2.set_title('Result', fontsize=40)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+#plt.show() # Uncomment if running on your local machine
+
+colorsel = color_thresh(warped, rgb_thresh=(160, 160, 160))
+
+# Plot the result
+plt.imshow(colorsel, cmap='gray')
+plt.show() 
+
+#get non_zeros pixel position from the binary image 
+
+ypos, xpos = colorsel.nonzero()
+plt.plot(xpos, ypos, '.')
+plt.xlim(0, 320)
+plt.ylim(0, 160)
+plt.show() 
+
+xpix, ypix = rover_coords(colorsel)
+
+fig = plt.figure(figsize=(5, 7.5))
+plt.plot(xpix, ypix, '.')
+plt.ylim(-160, 160)
+plt.xlim(0, 160)
+plt.title('Rover-Centric Map', fontsize=20)
+#plt.show() # Uncomment if running on your local machi
+
+xpix, ypix = rover_coords(colorsel)
+# Generate 200 x 200 pixel worldmap
+worldmap = np.zeros((200, 200))
+scale = 10
+# Get navigable pixel positions in world coords
+x_world, y_world = pix_to_world(xpix, ypix, rover_xpos, 
+                                rover_ypos, rover_yaw, 
+                                worldmap.shape[0], scale)
+# Add pixel positions to worldmap
+worldmap[y_world, x_world] += 1
+print('Xpos =', rover_xpos, 'Ypos =', rover_ypos, 'Yaw =', rover_yaw)
+# Plot the map in rover-centric coords
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 7))
+f.tight_layout()
+ax1.plot(xpix, ypix, '.')
+ax1.set_title('Rover Space', fontsize=40)
+ax1.set_ylim(-160, 160)
+ax1.set_xlim(0, 160)
+ax1.tick_params(labelsize=20)
+
+ax2.imshow(worldmap, cmap='gray')
+ax2.set_title('World Space', fontsize=40)
+ax2.set_ylim(0, 200)
+ax2.tick_params(labelsize=20)
+ax2.set_xlim(0, 200)
+
+
+plt.subplots_adjust(left=0.1, right=1, top=0.9, bottom=0.1)
+#plt.show() # Uncomment if running on your local machine
